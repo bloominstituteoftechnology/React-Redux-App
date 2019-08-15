07b8e77c2689e9cc7721fe3ea5f415145d53bbd5
Landry Irakoze <irakozel@gmail.com>
2019-08-15 16:17:47 -0400

    structured out app

---
+++ my_app/src/reducers/index.js
@@ -0,0 +1,1 @@
+export { newsReducer } from './newsReducer';
---
+++ my_app/src/reducers/newsReducer.js
@@ -0,0 +1,10 @@
+const initialState = [
+    
+]
+
+export const newsReducer = (state = initialState, action) => {
+    switch(action.type) {
+        default:
+            return state;
+    }
+}
