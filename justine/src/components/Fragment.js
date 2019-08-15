e6c22ff6bf06df330c2ba2f33cfa3a76f2b8111b
justinelai <justine.lai@gmail.com>
2019-08-15 17:41:42 -0400

    displaying img as background image

--- justine/src/components/Fragment.js
+++ justine/src/components/Fragment.js
@@ -2,14 +2,25 @@ import React from 'react'
 import styled, { css } from 'styled-components' 
 
 const Fragment = (props) => {
+    console.log(props)
 
     const Tile = styled.div`
-    background
-    `
+    background-image: url('${props => props.primaryimageurl}'), url('${props => props.primaryimageurl}');
+	background-size: cover, 5% 5%;
+    background-position: center;
+    background-blend-mode: overlay;
+    height:100vh;
+    `;
+
+    const Title = styled.h1`
+    
+    `;
 
     return(
         <div>
-            <img src={props.primaryimageurl}></img>
+            <Tile primaryimageurl={props.primaryimageurl}>
+                <Title>{props.title}</Title>
+            </Tile>
         </div>
     )
 }
