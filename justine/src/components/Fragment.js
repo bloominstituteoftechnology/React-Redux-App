d1e78760f6c789dd7027ab415010eb95a83739ff
justinelai <justine.lai@gmail.com>
2019-08-15 17:07:34 -0400

    install styled components

--- justine/src/components/Fragment.js
+++ justine/src/components/Fragment.js
@@ -1,6 +1,12 @@
 import React from 'react'
+import styled, { css } from 'styled-components' 
 
 const Fragment = (props) => {
+
+    const Tile = styled.div`
+    background
+    `
+
     return(
         <div>
             <img src={props.primaryimageurl}></img>
