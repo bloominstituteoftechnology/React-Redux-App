1a49faa7f1f57bdbb8535e607429eb65bb3cdba6
seongck <seongk@pm.me>
2019-09-27 16:49:38 -0700

    Wrap <App /> in <Provider /> and set up middleware

--- github-jobs/src/index.js
+++ github-jobs/src/index.js
@@ -1,10 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createStore, applyMiddleware } from 'redux';
+import { Provider } from 'react-redux';
+import logger from 'redux-logger'
+import thunk from 'redux-thunk';
+
 import './index.css';
 import App from './App';
+import reducer from './reducers';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const store = createStore(
+  reducer,
+  applyMiddleware(thunk, logger)
+)
+
+ReactDOM.render(
+  <Provider store={ store }>
+    <App />
+  </Provider>,
+  document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
