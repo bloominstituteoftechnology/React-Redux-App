17ad64c825293af6b8d44639776cb883184ee579
seongck <seongk@pm.me>
2019-09-27 17:07:42 -0700

    Connect JobList component and set up props and useEffect hook

---
+++ github-jobs/src/components/JobList.js
@@ -0,0 +1,34 @@
+import React, { useEffect } from 'react';
+import { connect } from 'react-redux';
+
+import Job from './Job';
+import { getJobs } from '../actions';
+
+function JobList({ getJobs, jobs, isFetching, error }) {
+  useEffect(() => {
+    getJobs();
+  }, [getJobs]);
+
+  if(isFetching) {
+    return <h2>... fetching jobs ...</h2>
+  }
+
+  return (
+    <div>
+      { jobs.map( job => (
+        <Job key={job.id} job={job} />
+      ))}
+    </div>
+  )
+}
+
+const mapStateToProps = state => ({
+  isFetching: state.isFetching,
+  jobs: state.jobs,
+  error: state.error
+})
+
+export default connect(
+  mapStateToProps,
+  { getJobs }
+)(JobList);
