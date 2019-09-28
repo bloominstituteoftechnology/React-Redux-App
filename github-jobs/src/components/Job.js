b8dd337aa7fe3c901d39a0e9e7e76bc34b3a579d
seongck <seongk@pm.me>
2019-09-27 18:04:09 -0700

    Update styles and classNames

--- github-jobs/src/components/Job.js
+++ github-jobs/src/components/Job.js
@@ -1,23 +1,39 @@
-import React from 'react';
+import React from "react";
 
 function Job({ job }) {
   return (
     <div className="job">
       <h4>{job.title}</h4>
-      { job.company_logo
-          ? <div><img
-            src={job.company_logo} 
+      {job.company_logo ? (
+        <div>
+          <img
+            src={job.company_logo}
             alt={`${job.company} logo`}
-            style={{ maxWidth: '250px' }}
-          /></div>
-        : null
-      }
-      <p><strong>Type:</strong> {job.type}</p>
-      <p>Company: {job.company}</p>
-      <p>Location: {job.location}</p>
-      <p>Description:</p>
-      {job.description}
-      {job.how_to_apply}
+            style={{ maxWidth: "250px" }}
+          />
+        </div>
+      ) : null}
+      <p>
+        <strong>Type:</strong> {job.type}
+      </p>
+      <p>
+        <strong>Company:</strong> {job.company}
+      </p>
+      <p>
+        <strong>Location:</strong> {job.location}
+      </p>
+      <p style={{ maxWidth: "100px" }}>
+        <strong>URL: </strong>
+        <a
+          href={`${job.url}`}
+          style={{
+            textDecoration: "none",
+            color: "inherit"
+          }}
+        >
+          {job.url}
+        </a>
+      </p>
     </div>
   );
 }
--- github-jobs/src/components/JobList.js
+++ github-jobs/src/components/JobList.js
@@ -1,32 +1,32 @@
-import React, { useEffect } from 'react';
-import { connect } from 'react-redux';
+import React, { useEffect } from "react";
+import { connect } from "react-redux";
 
-import Job from './Job';
-import { getJobs } from '../actions';
+import Job from "./Job";
+import { getJobs } from "../actions";
 
 function JobList({ getJobs, jobs, isFetching, error }) {
   useEffect(() => {
     getJobs();
   }, [getJobs]);
 
-  if(isFetching) {
-    return <h2 className="loading-header">... fetching jobs ...</h2>
+  if (isFetching) {
+    return <h2 className="loading-header">... fetching jobs ...</h2>;
   }
 
   return (
     <div className="joblist">
-      { jobs.map( job => (
+      {jobs.map(job => (
         <Job key={job.id} job={job} />
       ))}
     </div>
-  )
+  );
 }
 
 const mapStateToProps = state => ({
   isFetching: state.isFetching,
   jobs: state.jobs,
   error: state.error
-})
+});
 
 export default connect(
   mapStateToProps,
