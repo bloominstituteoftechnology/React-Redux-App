cc4e86da2015b50a963769b62e8ecc893c7c04d1
seongck <seongk@pm.me>
2019-09-27 17:06:34 -0700

    Import and render JobList component

--- github-jobs/src/App.js
+++ github-jobs/src/App.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import './App.css';
 
+import JobList from './components/JobList';
+
 function App() {
   return (
     <div className="App">
       <h1>Github Jobs</h1>
+      <JobList />
     </div>
   );
 }
