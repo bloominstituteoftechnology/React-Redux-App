12f638c8191b80cd2248d120791043a2e67eb86f
Kevin Allen <kevin.allen91@gmail.com>
2019-10-10 12:48:54 -0700

    data loading onto app

--- react-redux-app/src/App.js
+++ react-redux-app/src/App.js
@@ -1,11 +1,12 @@
 import React from 'react';
-
 import './App.css';
+import TeamPage from './components/teamPage'
 
 function App() {
   return (
     <div className="App">
-      TEST
+      <h1>MLB TEAMS</h1>
+      <TeamPage />
     </div>
   );
 }
--- react-redux-app/src/reducers/index.js
+++ react-redux-app/src/reducers/index.js
@@ -1,3 +1,5 @@
+import { START_FETCHING, FETCH_SUCCESS, FETCH_FAILURE } from "../actions"
+
 const initialState = {
     mlbData: [],
     isLoading: false,
@@ -6,7 +8,29 @@ const initialState = {
 
 const reducer = (state = initialState, action) => {
     switch(action.type) {
+        case START_FETCHING:
+            return{
+                ...state,
+                isLoading:true,
+                error:''
+            }
+        case FETCH_SUCCESS:
+            return {
+                ...state,
+                isLoading:false,
+                mlbData: action.payload,
+                error: ''
+            }
+        case FETCH_FAILURE:
+            return {
+                ...state,
+                isLoading:false,
+                error: action.payload
+            }    
+        
         default:
             return state
     }
 }
+
+export default reducer
