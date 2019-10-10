2a64cc0ea96de3254d8fb2716f28eed13ce04422
Kevin Allen <kevin.allen91@gmail.com>
2019-10-10 11:53:20 -0700

    initial commit

---
+++ react-redux-app/src/reducers/index.js
@@ -0,0 +1,12 @@
+const initialState = {
+    mlbData: [],
+    isLoading: false,
+    error: '',
+}
+
+const reducer = (state = initialState, action) => {
+    switch(action.type) {
+        default:
+            return state
+    }
+}
