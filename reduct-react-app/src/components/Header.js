12b99c82be4ee512fdbcc20803b9c048056ed251
dangus1924 <51095136+dangus1924@users.noreply.github.com>
2019-10-24 18:07:10 -0400

    added Header component

---
+++ reduct-react-app/src/components/Header.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function Header() {
+    return (
+        <div>
+            <h1>Welcome User </h1>
+            <p>Let us see if you are qualified to make it</p>
+        </div>
+        
+    )
+}
+
+export default Header;
