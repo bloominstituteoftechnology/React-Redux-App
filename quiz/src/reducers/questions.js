492d547028c98f9814793e958c69a8243bdbe416
Michelle Scott <scottmm374@gmail.com>
2019-10-24 16:47:38 -0400

    set up reducer for api call

---
+++ quiz/src/reducers/questions.js
@@ -0,0 +1,35 @@
+import {
+  FETCH_QUESTIONS_START,
+  FETCH_QUESTIONS_SUCCESS,
+  FETCH_QUESTIONS_FAILED
+} from "../actions/questions";
+
+const initialState = {
+  quiz: [],
+  isLoading: false,
+  error: null
+};
+
+export function reducer(state = initialState, action) {
+  switch (action.type) {
+    case FETCH_QUESTIONS_START:
+      return {
+        ...state,
+        isLoading: true
+      };
+    case FETCH_QUESTIONS_SUCCESS:
+      return {
+        ...state,
+        quiz: [...state.results, ...action.payload],
+        isLoading: false
+      };
+    case FETCH_QUESTIONS_FAILED:
+      return {
+        ...state,
+        error: action.payload,
+        isLoading: false
+      };
+    default:
+      return state;
+  }
+}
