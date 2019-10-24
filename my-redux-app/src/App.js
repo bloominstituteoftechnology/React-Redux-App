aaabbeb59614e88f338e7a00c9d250c8cbd647b1
spettigrew <sara.pettigrew5@gmail.com>
2019-10-24 06:51:35 -0600

    some folders set up and some packaging installed

---
+++ my-redux-app/src/App.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        
+      </header>
+    </div>
+  );
+}
+
+export default App;
+
+
+// https://lyricsovh.docs.apiary.io/#reference/0/lyrics-of-a-song/search?console=1
+// https://api.lyrics.ovh/
+// https://picsum.photos/seed/picsum/200/300
