8265ca6889b34134ba365060e137f2f28d509b8f
Michelle Scott <scottmm374@gmail.com>
2019-10-24 16:36:14 -0400

    set up actions for fetching data

---
+++ quiz/src/actions/questions.js
@@ -0,0 +1,21 @@
+import axios from "axios";
+export const FETCH_QUESTIONS_START = "FETCH_QUESTIONS_START";
+export const FETCH_QUESTIONS_SUCCESS = "FETCH_QUESTIONS_SUCCESS";
+export const FETCH_QUESTIONS_FAILED = "FETCH_QUESTIONS_FAILED";
+
+export function fetchQuestions() {
+  return dispatch => {
+    dispatch({ type: FETCH_QUESTIONS_START });
+
+    axios
+      .get(
+        "https://opentdb.com/api.php?amount=10&category=9&difficulty=easy&type=boolean"
+      )
+      .then(res => {
+        dispatch({ type: FETCH_QUESTIONS_SUCCESS, payload: res.data.results });
+      })
+      .catch(err => {
+        dispatch({ type: FETCH_QUESTIONS_FAILED, payload: err });
+      });
+  };
+}
--- quiz/src/index.js
+++ quiz/src/index.js
@@ -1,12 +1,25 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import { createStore, applyMiddleware } from "redux";
+import { Provider } from "react-redux";
+import thunk from "redux-thunk";
+import logger from "redux-logger";
+import "./index.css";
+import App from "./App";
+import { reducer } from "./reducers";
+import * as serviceWorker from "./serviceWorker";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const store = createStore(
+  reducer,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__,
+  applyMiddleware(thunk, logger)
+);
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById("root")
+);
+
 serviceWorker.unregister();
