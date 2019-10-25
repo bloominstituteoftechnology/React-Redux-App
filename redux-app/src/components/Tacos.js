574ace08a3030dfd6444e9003b1b2d8c5f7b04e4
bobbidigi <bobbidigi34@gmail.com>
2019-10-24 23:11:35 -0400

    add placekeanuea and useeffect for details

--- redux-app/src/components/Tacos.js
+++ redux-app/src/components/Tacos.js
@@ -1,14 +1,14 @@
 import React from 'react'
+import Taco from './Taco'
 
 export default function DogList(props) {
-    console.log("Dogs", props)
+    console.log("Tacos", props)
     return (
         <div>
-            <h1>Tacos</h1>
+            <h1>Detroit Tacos</h1>
 
-                {props.venues.map((item) => {
-                return <h1>{item.name}</h1>
-                })}
+                {props.venues.map((item, index) => (<Taco taco={item} key={index}/>)
+                )}
         </div>
     )
 }
