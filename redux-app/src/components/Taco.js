771a3151ae9d050da1fca5bdfd0b94e12bf5f3d3
bobbidigi <bobbidigi34@gmail.com>
2019-10-26 13:27:27 -0400

    add error handling for no icons

--- redux-app/src/components/Taco.js
+++ redux-app/src/components/Taco.js
@@ -7,27 +7,28 @@ export default function Taco(props) {
 const REACT_APP_ID = process.env.REACT_APP_ID;
 const REACT_APP_SECRET = process.env.REACT_APP_SECRET;
 console.log("Taco", props)
-console.log("categories", props.taco.categories[0].icon)
-const image = props.taco.categories[0].icon;
+console.log("categories", props.taco.categories[0])
+// const image = ;
 
-// useEffect(()=>{
-//     axios
-//     .get(`https://api.foursquare.com/v2/venues/${props.taco.id}?client_id=${REACT_APP_ID}&client_secret=${REACT_APP_SECRET}&v=20180323`)
-//     .then(res => {
-//         console.log("image", res.data.response.venue.bestPhoto)
-//         setImage(`${res.data.response.venue.prefix}300x500${res.data.response.venue.suffix}`)
-//     })
-// },[])
+const [newImage, setImg] = useState('https://picsum.photos/64/64?random=1?grayscale')
+
+useEffect(()=>{
+    if(props.taco.categories[0]){
+        setImg(`${props.taco.categories[0].icon.prefix}64${props.taco.categories[0].icon.suffix}`)
+    }else{
+        console.log(props.taco)
+    }
+},[])
 
 // To assemble a photo URL, combine the response’s prefix + size + suffix. 
 // Example: https://igx.4sqi.net/img/general/300x500/5163668_xXFcZo7sU8aa1ZMhiQ2kIP7NllD48m7qsSwr1mJnFj4.jpg
 
     return (
         <div className="card">
-            <img className="icon" src={`${image.prefix}64${image.suffix}`}/>
+            <img className="icon" src={newImage}/>
             <div className="info">
                 <h2>{props.taco.name}</h2>
-                <h3>{props.taco.location.formattedAddress[0]}</h3>
+                <h4>{props.taco.location.formattedAddress[0]}</h4>
             </div>
         </div>
     )
