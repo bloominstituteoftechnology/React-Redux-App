f4cc40f49e183e9d577689f493c5459e1c348fae
Leighton Fritze <Jay.Row.Fritze@gmail.com>
2019-11-07 11:46:52 -0500

    charts setup and rendering

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,11 +1,14 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Charts from "./components/Charts";
+import NavBar from "./components/NavBar";
 
 function App() {
   return (
     <div className="App">
-      Nothing Yet
+      <NavBar />
+      <h1>Coin MarketSnap</h1>
+      <Charts />
     </div>
   );
 }
---
+++ my-app/src/components/NavBar.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+const NavBar = () => {
+  return (
+    <div className="nav-bar">
+      <div className="logo">
+        <a href="#">CMS</a>
+      </div>
+      <div className="links">
+        <a href="#">Home</a>
+        <a href="#">Contact</a>
+        <a href="#">Coins</a>
+        <a href="#">About</a>
+      </div>
+    </div>
+  );
+};
+
+export default NavBar;
