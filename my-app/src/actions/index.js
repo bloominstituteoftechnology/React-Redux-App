9ad54ffbbcfba662b66acdd7ccbfdb23617b4d1c
Leighton Fritze <Jay.Row.Fritze@gmail.com>
2019-11-07 11:27:13 -0500

    setup reducer state and actions

---
+++ my-app/src/actions/index.js
@@ -0,0 +1,17 @@
+import axios from 'axios';
+
+export const START_FETCHING = 'START_FETCHING';
+export const FETCH_SUCCESS = 'FETCH_SUCCESS';
+export const FETCH_FAILURE ='FETCH_FAILURE';
+
+export const fetchCoinData = () => dispatch => {
+
+    dispatch({ type: START_FETCHING });
+
+    axios
+     .get('https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=10&page=1&sparkline=true')
+     .then(response => dispatch({ type: FETCH_SUCCESS, payload: response.data }))
+     .catch(error => dispatch({ type: FETCH_FAILURE, payload: error.response }));
+}
+
+// https://api.coingecko.com/api/v3/exchanges/{id}/volume_chart
--- my-app/src/index.js
+++ my-app/src/index.js
@@ -7,6 +7,8 @@ import { createStore, applyMiddleware, compose } from 'redux';
 import { Provider } from 'react-redux';
 import thunk from 'redux-thunk';
 
+import rootReducer from './reducers';
+
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 const store = createStore(
     rootReducer,
