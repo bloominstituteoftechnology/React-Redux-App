0b07134a53bcd57391d6850260c9e917b8e5dfcd
Leighton Fritze <Jay.Row.Fritze@gmail.com>
2019-11-07 13:34:21 -0500

    setup PlayerCard and connect react-redux

--- my-app/src/components/PlayerCard.js
+++ my-app/src/components/PlayerCard.js
@@ -1,4 +1,35 @@
-// import React, { useEffect } from "react";
-// import BasicCard from "./BasicCard;
-// import { connect } from "react-redux";
+import React, { useEffect } from "react";
+import { connect } from "react-redux";
+import BasicCard from './BasicCard';
+import { fetchPlayerData } from '../actions';
 
+const PlayerCard = props => {
+    useEffect(() => {
+        props.fetchPlayerData();
+    }, []);
+
+    if (props.isFetching){
+        return <h3>...Loading Info</h3>
+    }
+    console.log(props);
+    return (
+        <div className ='player-card'>
+            {props.error && <p>{props.error}</p>}
+            {props.players.map(player => (
+                <BasicCard key={player.id} player={player} />
+        ))}
+        </div>
+    )
+}
+
+const mapStateToProps = state => {
+    return {
+        players: state.players,
+        isFetching: state.isFetching,
+        error: state.error
+    };
+};
+
+export default connect(
+    mapStateToProps, { fetchPlayerData }
+) (PlayerCard);
