eaf18bc0698643286550dd04da5a83e8735c306a
Leighton Fritze <Jay.Row.Fritze@gmail.com>
2019-11-07 13:02:04 -0500

    setup reducer state

--- my-app/src/reducers/index.js
+++ my-app/src/reducers/index.js
@@ -1,2 +1,36 @@
-// import { START_FETCHING, FETCH_SUCCESS, FETCH_FAILURE } from '../actions';
+import { START_FETCHING, FETCH_SUCCESS, FETCH_FAILURE } from '../actions';
+
+const initialState = {
+    players: [],
+    isFetching: false,
+    error: ''
+};
+
+const reducer = (state = initialState, action) => {
+    switch (action.type) {
+        case START_FETCHING:
+            return {
+                ...state,
+                isFetching: true,
+                error: ''
+            };
+        case FETCH_SUCCESS:
+            return {
+                ...state,
+                isFetching: false,
+                error: '',
+                players: action.payload
+            };
+        case FETCH_FAILURE:
+            return {
+                ...state,
+                isFetching: false,
+                error: action.payload
+            };
+        default:
+            return state;
+    }
+}
+
+export default reducer;
 
