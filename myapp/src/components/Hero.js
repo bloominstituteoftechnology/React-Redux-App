610200766055340043ac429007b69e77ca814191
Forrest-Bingham <bingham_5@live.com>
2019-11-08 15:43:00 -0700

    Forrest-Bingham

--- myapp/src/components/Hero.js
+++ myapp/src/components/Hero.js
@@ -2,10 +2,22 @@ import React from "react";
 import { connect } from "react-redux";
 
 const HeroStats = props => {
+  const setAttrClass = attr => {
+    switch (attr) {
+      case "str":
+        return "primary_attr-str";
+      case "agi":
+        return "primary_attr-agi";
+      case "int":
+        return "primary_attr-int";
+      default:
+        return "primary_attr-str";
+    }
+  };
   return (
     <div className="Hero">
       <h2> {props.hero.localized_name}</h2>
-      <h3 className={`primary_attr${props.hero.primary_attr ? " str" : "agi"}`}>
+      <h3 className={setAttrClass(props.hero.primary_attr)}>
         {`${props.hero.primary_attr}`.toUpperCase()}
       </h3>
       <h3 className="attackType">{props.hero.attack_type}</h3>
