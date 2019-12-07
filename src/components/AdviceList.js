dd21846bc554592f24d3c1a8d5801f24f835932e
Tisha Holder <50963507+TishaHolder@users.noreply.github.com>
2019-12-07 15:21:54 -0500

    added animations

--- src/components/AdviceList.js
+++ src/components/AdviceList.js
@@ -1,9 +1,45 @@
 import React, { useEffect } from "react";
+import { bounce, zoomInLeft, swing, slideInRight, tada, rubberBand, slideInLeft, rollIn, fadeInDownBig, hinge } from 'react-animations';
+import styled, { keyframes } from "styled-components";
 import { connect } from "react-redux";
 
 import { getAdvice } from "../actions/actions.js";
 
+//styled components with 10 different animations
+const Bounce = styled.div`animation: 2s ${keyframes `${bounce}`} `;
+const ZoomInLeft = styled.div`animation: 2s ${keyframes `${zoomInLeft}`} `;
+const Swing = styled.div`animation: 2s ${keyframes `${swing}`} `;
+const SlideInRight = styled.div`animation: 2s ${keyframes `${slideInRight}`} `;
+const SlideInLeft = styled.div`animation: 2s ${keyframes `${slideInLeft}`} `;
+const Tada = styled.div`animation: 2s ${keyframes `${tada}`} `;
+const RubberBand = styled.div`animation: 2s ${keyframes `${rubberBand}`} `;
+const RollIn = styled.div`animation: 2s ${keyframes `${rollIn}`} `;
+const FadeIn = styled.div`animation: 2s ${keyframes `${fadeInDownBig}`} `;
+const Hinge = styled.div`animation: 2s ${keyframes `${hinge}`} `;
+
 function AdviceList(props) {
+
+  //array of animations
+  const animationArray = [
+    Bounce,
+    ZoomInLeft,
+    Swing,
+    SlideInRight,
+    SlideInLeft,
+    Tada,
+    RubberBand,
+    RollIn,
+    FadeIn
+  ]
+
+  //array index generated using math.floor and the lenght of the animatiion array
+  //Use Math.floor() to get the index ranging from(0 to arrayLength-1).
+  var arrayIndex = Math.floor(Math.random() * animationArray.length);
+
+  //stores the name of each randomly selected component
+  //used below instead of the direct name of a styled component
+  //this produces a different animation every time the "i need advice" button is selected
+  var AnimationComponent = animationArray[arrayIndex];
 
     useEffect(() => {
 
@@ -18,16 +54,18 @@ function AdviceList(props) {
       
       console.log("advice here: ", props.advice);
     
-      return (
+      return (  
         <>
-        <div className = "todo-list-div">
-          <h2>{props.advice}</h2>
-        </div>
+       
+          <div className = "todo-list-div">
+            <AnimationComponent>
+              <h2>{props.advice}</h2>
+            </AnimationComponent>
+          </div>        
 
-        <div className = "generator-button-div">
-          
-          <button onClick={props.getAdvice}>I Need Advice</button>
-        </div>
+          <div className = "generator-button-div">            
+            <button onClick={props.getAdvice}>I Need Advice</button>
+          </div>
 
         </>
       );  
