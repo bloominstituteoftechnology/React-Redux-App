ba6768ef3e617faf3a86a0627f5849a7759ac422
toneiojimz <ajimeneza77@gmail.com>
2019-12-12 14:44:12 -0800

    created store

--- app/src/index.js
+++ app/src/index.js
@@ -2,11 +2,25 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import * as serviceWorker from './serviceWorker';
+import { createStore, applyMiddleware, compose} from 'redux';
+import {Provider} from 'react-redux';
+import thunk from 'redux-thunk';
+import logger from 'redux-logger';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import reducer from './reducers/nameReducer';
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+const store = createStore (
+    reducer,
+    composeEnhancers(applyMiddleware(thunk, logger))
+);
+
+
+const rootElement = document.getElementById('root');
+ReactDOM.render(
+    <Provider store={store}>
+        <App/>
+    </Provider>,
+    rootElement
+);
--- app/src/reducers/nameReducer.js
+++ app/src/reducers/nameReducer.js
@@ -10,7 +10,7 @@ const initialState = {
 };
 
 const reducer = (state = initialState, action) => {
-    switch(action, type){
+    switch(action.type){
         case FETCH_NAME_START:
             return{
                 ...state,
