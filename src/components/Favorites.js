06a980d1b847036998b9ff4e3570fef20978b5da
tygedavis <tyge.davis@gmail.com>
2019-12-13 15:48:37 -0700

    Added quotes to the favorites quotes

--- src/components/Favorites.js
+++ src/components/Favorites.js
@@ -3,8 +3,8 @@ import React from 'react';
 const Favorites = props => {
   // console.log('Props Favorites: ', props)
   return (
-    <li>
-      {props.quote}
+    <li className='quotes'>
+      "{props.quote}"
     </li>
   )
 }
