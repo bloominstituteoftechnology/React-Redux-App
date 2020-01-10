ce782ea7bb98d903f4536bb138054cdc88e15ac4
chiragthesia <harithesia@gmail.com>
2020-01-09 20:08:56 -0600

    Redux primary setup done

--- react-redux-test/src/index.js
+++ react-redux-test/src/index.js
@@ -1,12 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import thunk from "redux-thunk";
+import { createStore, applyMiddleware } from "redux";
+import { Provider } from "react-redux";
+
 import './index.css';
 import App from './App';
-import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import stockReducer from "./actions/stockAction";
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+const store = createStore(stockReducer, applyMiddleware(thunk));
+
+const rootElement = document.getElementById('root')
+ReactDOM.render(
+    <Provider store ={ store }>
+        <App />
+    </Provider>,
+    rootElement 
+);
