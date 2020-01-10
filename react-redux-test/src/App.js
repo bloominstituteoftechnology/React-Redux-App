2effa5e6c2889553cb2bb5284b20041bd9d61028
chiragthesia <harithesia@gmail.com>
2020-01-09 18:33:19 -0600

    Initial Commit

---
+++ react-redux-test/src/App.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
