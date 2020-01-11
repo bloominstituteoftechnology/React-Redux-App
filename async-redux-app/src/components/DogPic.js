822588ebdb876545e79b6a73cc064108b4db1f7d
Trey McGarity <trey.mcgarity99@gmail.com>
2020-01-11 00:09:35 -0600

    actual mvp, finalized

--- async-redux-app/src/components/DogPic.js
+++ async-redux-app/src/components/DogPic.js
@@ -4,13 +4,12 @@ import { getDog } from '../actions/actions';
 import './DogPic.css';
 
 const DogPic = props => {
-    console.log("PROPS: ",props);
     return (
         <div className="dog-picture">
             {props.loading ? (
                 <h1>Loading</h1>
             ) : (
-                <img style={{ maxWidth: "300px"}} src={props.dog} alt="dog" />
+                <img className="dog-image" src={props.dog} alt="dog" />
             )}
             <button className="new-dog-button" onClick={() => {props.getDog()}}>Get Dog</button>
         </div>
@@ -18,7 +17,6 @@ const DogPic = props => {
 }
 
 const mapStateToProps = state => {
-    console.log("state.dog = ",state.dog);
     return { loading: state.loading, dog: state.dog };
 }
 
--- async-redux-app/src/reducers/dogPicReducer.js
+++ async-redux-app/src/reducers/dogPicReducer.js
@@ -1,15 +1,12 @@
 const initialState = {
-    editing: false,
     loading: false,
     dog: 'https://images.dog.ceo/breeds/hound-basset/n02088238_9815.jpg'
 };
 
 export const dogPicReducer = (state = initialState, action) => {
     switch(action.type) {
-        case 'EDITING': return { ...state, editing: true };
-        case 'NOT_EDITING': return { ...state, editing: false };
         case 'LOADING': return { ...state, loading: true };
-        case 'NEW_DOG': console.log("action.payload = ",action.payload); return { ...state, loading: false, dog: action.playload };
+        case 'NEW_DOG': return { ...state, loading: false, dog: action.payload };
         default: return state;
     }
 };
