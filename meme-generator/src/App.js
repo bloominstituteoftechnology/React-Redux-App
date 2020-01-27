c58da1d889764c03f3a00e44ed04e1ed99c26908
Ana Zoe <50473626+acarrillo3@users.noreply.github.com>
2020-01-27 10:37:46 -0500

    setting log IN LOG UP

--- meme-generator/src/App.js
+++ meme-generator/src/App.js
@@ -1,9 +1,17 @@
 import React, { useState, useEffect } from "react";
 import { Meme } from "./components/Meme";
 
+const objectToQueryParam = (obj) => {
+  const params = Object.entries(obj).map(([key, value]) => `${key}=${value}`)
+  return '?' + params.join('&')
+}
+
 function App() {
   const [templates, setTemplates] = useState([]);
   const [template, setTemplate] = useState(null);
+
+  const [topText, setTopText] = useState('');
+  const {bottomText, setBottomText} = useState('');
 
   useEffect(() => {
     fetch("https://api.imgflip.com/get_memes").then(x =>
@@ -13,13 +21,35 @@ function App() {
   return (
     <div style={{ textAlign: "center" }}>
       {template && (
-        <form onSubmit={e => {
+        <form onSubmit={async e => {
           e.preventDefault()
           //add logic to create meme from the api
+          const params = {
+            template_id: template.id,
+            text0: topText,
+            text1: bottomText,
+            username: process.env.REACT_APP_IMGFLIP_USERNAME,
+            password: process.env.REACT_APP_IMGFLIP_PASSWORD
+          }
+          const response = await fetch(`
+            https://api.imgflip.com/caption_image${objectToQueryParam(
+              params
+            )}`
+          );
+          const data = await response.json()
+          console.log(data);
         }}>
           <Meme template={template} />
-          <input placeholder="top text" />
-          <input placeholder="bottom text" />
+          <input 
+            placeholder="top text" 
+            value={topText} 
+            onChange={e => setTopText(e.target.value)} 
+          />
+          <input 
+            placeholder="bottom text" 
+            value={bottomText}
+            onChange={e => setBottomText(e.target.value)}  
+          />
           <button type="submit">create meme</button>
         </form>
       )} 
