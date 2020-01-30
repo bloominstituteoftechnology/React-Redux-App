2b57ef84f2fb1f5620cd5484bef3668e3f015c8f
Ana Zoe <50473626+acarrillo3@users.noreply.github.com>
2020-01-30 16:20:45 -0500

    fully functional to update the bottom and top text

--- meme-generator/src/App.js
+++ meme-generator/src/App.js
@@ -1,65 +1,75 @@
 import React, { useState, useEffect } from "react";
 import { Meme } from "./components/Meme";
 
-const objectToQueryParam = (obj) => {
-  const params = Object.entries(obj).map(([key, value]) => `${key}=${value}`)
-  return '?' + params.join('&')
-}
+const objectToQueryParam = obj => {
+  const params = Object.entries(obj).map(([key, value]) => `${key}=${value}`);
+  return "?" + params.join("&");
+};
 
 function App() {
   const [templates, setTemplates] = useState([]);
   const [template, setTemplate] = useState(null);
-
-  const [topText, setTopText] = useState('');
-  const {bottomText, setBottomText} = useState('');
+  const [topText, setTopText] = useState("");
+  const [bottomText, setBottomText] = useState("");
+  const [meme, setMeme] = useState(null);
 
   useEffect(() => {
     fetch("https://api.imgflip.com/get_memes").then(x =>
       x.json().then(response => setTemplates(response.data.memes))
     );
   }, []);
+
+  if (meme) {
+    return (
+      <div style={{ textAlign: "center" }}>
+        <img style={{ width: 200 }} src={meme} alt="custom meme" />
+      </div>
+    );
+  }
+
   return (
     <div style={{ textAlign: "center" }}>
       {template && (
-        <form onSubmit={async e => {
-          e.preventDefault()
-          //add logic to create meme from the api
-          const params = {
-            template_id: template.id,
-            text0: topText,
-            text1: bottomText,
-            username: process.env.REACT_APP_IMGFLIP_USERNAME,
-            password: process.env.REACT_APP_IMGFLIP_PASSWORD
-          }
-          const response = await fetch(`
-            https://api.imgflip.com/caption_image${objectToQueryParam(
-              params
-            )}`
-          );
-          const data = await response.json()
-          console.log(data);
-        }}>
+        <form
+          onSubmit={async e => {
+            e.preventDefault();
+            // add logic to create meme from api
+            const params = {
+              template_id: template.id,
+              text0: topText,
+              text1: bottomText,
+              username: "xzk03017",
+              password: "xzk03017@cndps.com"
+            };
+            const response = await fetch(
+              `https://api.imgflip.com/caption_image${objectToQueryParam(
+                params
+              )}`
+            );
+            const json = await response.json();
+            setMeme(json.data.url);
+          }}
+        >
           <Meme template={template} />
-          <input 
-            placeholder="top text" 
-            value={topText} 
-            onChange={e => setTopText(e.target.value)} 
+          <input
+            placeholder="top text"
+            value={topText}
+            onChange={e => setTopText(e.target.value)}
           />
-          <input 
-            placeholder="bottom text" 
+          <input
+            placeholder="bottom text"
             value={bottomText}
-            onChange={e => setBottomText(e.target.value)}  
+            onChange={e => setBottomText(e.target.value)}
           />
           <button type="submit">create meme</button>
         </form>
-      )} 
-      {!template && (//we are displaying a list of templates until the user clicks on a meme => that will update the state and it will set the template/meme
+      )}
+      {!template && (
         <>
-          <h1>Pick a Meme</h1>
+          <h1>Pick a template</h1>
           {templates.map(template => {
-          //if not template/meme selected show them the options
             return (
-              <Meme //basically we extracted the image logic into its own component that way we can use that twice <Meme/>. 
+              <Meme
                 template={template}
                 onClick={() => {
                   setTemplate(template);
