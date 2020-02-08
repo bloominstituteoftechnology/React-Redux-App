cb9ad850b630c6d18a10ee8edf1d067f85b8b3a5
Kristi Gribble <kgribble4083@gmail.com>
2020-02-08 14:51:06 -0600

    styled cards

--- react-redux-app/src/components/CardList.js
+++ react-redux-app/src/components/CardList.js
@@ -2,13 +2,30 @@ import React from "react";
 import { connect } from "react-redux";
 import { getData } from "../actions";
 
+
 const CardList = (props) => {
   return (
     <>
       {props.error ? (
         <div>{props.error}</div>
       ) : (
-        props.data.map(pokemon => <div><img src={pokemon.imageUrl}></img></div>)
+        props.data.map(pokemon => 
+        <div className="card" key={pokemon.id}>
+          <div className="row">
+            <div className="column">
+              <img src={pokemon.imageUrl} alt="Pokemon Card"></img>
+            </div>
+          <div className="column">
+            <div >
+              <h3>{pokemon.name}</h3>
+                <p>Number: {pokemon.number}</p>
+                <p>Rarity: {pokemon.rarity}</p>
+                <p>Type: {pokemon.subtype}</p>
+            </div>
+          </div>
+        </div>
+       
+        </div>)
       )}
     </>
   );
