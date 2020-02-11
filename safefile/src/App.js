97932c07f6d3884999db4575b0a441033a9c876f
dangolbeeker <scottb305@outlook.com>
2020-02-11 02:28:44 -0500

    CRA

---
+++ safefile/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      Start 
+    
+    </div>
+  );
+}
+
+export default App;
