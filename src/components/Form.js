f4d2f8c0a608b109ed949f7899709a99575043e3
Mandi Haase <haase1020@gmail.com>
2020-02-20 04:32:51 -0500

    fill out Form.js in components folder

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,12 +1,31 @@
 import React from 'react';
-import { Card } from 'semantic-ui-react';
+import { connect } from 'react-redux';
 
-const Breweries = props => {
-    return (
-        <Card.Group>
-            <Card>
-                
-            </Card>
-        </Card.Group>
-    )
-}
+import { getData } from '../actions';
+
+const Form = props => {
+    const handleGetData = e => {
+        e.preventDefault();
+        props.getData();
+    };
+return (
+    <>
+        {props.isFetchingData ? (
+            <div> Getting Data</div>
+
+        ): (
+            <button onClick={handleGetData}>Get Data</button>
+        )}
+        </>
+    );
+};
+const mapStateToProps = state => {
+    return {
+        isFetchingData: state.isFetchingData
+    };
+};
+
+export default connect(
+    mapStateToProps,
+    { getData }
+)(Form);
