b50cbe9c2fe23f62777145ddb5fb05b493b71d40
Mandi Haase <haase1020@gmail.com>
2020-02-20 04:47:05 -0500

    fill in List.js in components folder

---
+++ src/components/List.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import { connect } from 'react-redux';
+
+const List = props => {
+    return (
+        <>
+        {props.error ? (
+            <div className='error'>{props.error}</div>
+        ) : (
+            props.missions.map(mission => <div>{mission.mission_name}</div>)
+        )}
+        </>
+    )
+};
+const mapStateToProps = state => {
+    return {
+        missions: state.missions,
+        error: state.error
+    };
+};
+export default connect(
+    mapStateToProps,
+    {}
+)(MissionsList);
