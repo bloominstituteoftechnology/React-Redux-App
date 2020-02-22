7314e4171dde0fccd968383a53ed2e058fa5fa36
Mandi Haase <haase1020@gmail.com>
2020-02-22 15:47:37 -0500

    update actions/index.js

--- src/store/actions/index.js
+++ src/store/actions/index.js
@@ -1,8 +1,8 @@
 import axios from 'axios';
 
-export const FETCH_JOKE_START = 'FETCH_JOKE_START';
-export const FETCH_JOKE_SUCCESS = 'FETCH_JOKE_SUCCESS';
-export const FETCH_JOKE_FAILURE = 'FETCH_JOKE_FAILURE';
+// export const FETCH_JOKE_START = 'FETCH_JOKE_START';
+// export const FETCH_JOKE_SUCCESS = 'FETCH_JOKE_SUCCESS';
+// export const FETCH_JOKE_FAILURE = 'FETCH_JOKE_FAILURE';
 
 export const fetchJoke = (
   firstName = 'Chuck',
@@ -11,9 +11,11 @@ export const fetchJoke = (
   dispatch({ type: FETCH_JOKE_START });
   // fetch joke
   axios
-    .get(`//api.icndb.com/jokes/random?exclude=[explicit]`)
+    .get('https://quote-garden.herokuapp.com/quotes/random')
     .then(res => {
-      dispatch({ type: FETCH_JOKE_SUCCESS, payload: res.data.value.joke });
+
+      console.log('reponse', res)
+      // dispatch({ type: FETCH_JOKE_SUCCESS, payload: res.data.value.joke });
     })
     .catch(err => console.log(err.response));
 };
