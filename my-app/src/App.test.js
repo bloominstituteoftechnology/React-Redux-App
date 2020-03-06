8dced5a792d9a674ea4dc099b49d83f6112d13cb
Michael Lowe <mrlowememphis@yahoo.com>
2020-03-06 12:35:47 -0600

    Files cleaned up

--- my-app/src/App.test.js
+++ my-app/src/App.test.js
@@ -9,12 +9,3 @@ test('renders learn react link', () => {
 });
 
 
-// import React from 'react';
-// import { render } from '@testing-library/react';
-// import App from './App';
-
-// test('renders learn react link', () => {
-//   const { getByText } = render(<App />);
-//   const linkElement = getByText(/learn react/i);
-//   expect(linkElement).toBeInTheDocument();
-// });
