05112e40e13c8d008e999fc5688d490456dbca6f
Your name <a.m.verdine@gmail.com>
2020-03-07 12:02:48 -0500

    Update AppPostForm with submit handler/set state for submit handler/build post request and JSON FETCH response

--- app/src/components/AppPostForm.js
+++ app/src/components/AppPostForm.js
@@ -8,17 +8,37 @@ class AppPostForm extends Component {
       body: ""
     };
     this.onChange = this.onChange.bind(this);
+    this.onSubmit = this.onSubmit.bind(this);
   }
 
   onChange(e) {
     this.setState({ [e.target.name]: e.target.value });
   }
 
+  onSubmit(e) {
+    e.preventDefault();
+
+    const post = {
+      titile: this.state.title,
+      body: this.state.body
+    };
+
+    fetch("https://jsonplaceholder.typicode.com/posts", {
+      method: "POST",
+      headers: {
+        "content-type": "application/json"
+      },
+      body: JSON.stringify(post)
+    })
+      .then(res => res.json())
+      .then(data => console.log(data));
+  }
+
   render() {
     return (
       <div>
         <h1>Add Post</h1>
-        <form>
+        <form onSubmit={this.onSubmit}>
           <div>
             <label>Title: </label>
             <br />
