1f37c7999a1b602c09ab2d496723054f64c7bbe1
Your name <a.m.verdine@gmail.com>
2020-03-07 13:25:17 -0500

    Create reducers file/create actions file

---
+++ app/src/reducers/postReducer.js
@@ -0,0 +1,13 @@
+import { FETCH_POSTS, NEW_POST } from "../actions/types";
+
+const initialState = {
+  items: [],
+  item: {}
+};
+
+export default function(state = initialState, action) {
+  switch (action.type) {
+    default:
+      return state;
+  }
+}
