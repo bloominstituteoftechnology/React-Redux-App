d00915b6641dc8956bcf5f7f191dde0f6b27e7bc
Your name <a.m.verdine@gmail.com>
2020-03-07 13:54:22 -0500

    Import PropTypes

--- app/src/components/AppPosts.js
+++ app/src/components/AppPosts.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import PropTypes from "prop-types";
 import { connect } from "react-redux";
 import { fetchPosts } from "../actions/postActions";
 
@@ -23,6 +24,11 @@ class AppPosts extends Component {
     );
   }
 }
+
+Posts.propTypes = {
+  fetchPosts: PropTypes.func.isRequired,
+  posts: PropTypes.array.isRequired
+};
 
 const mapStateToProps = state => ({
   posts: state.posts.items
