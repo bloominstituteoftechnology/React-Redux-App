926f9117885c352fe06ca504fea07f556f93e0d0
Your name <a.m.verdine@gmail.com>
2020-03-07 13:17:55 -0500

    Create store in store file and import thunk and middleware

--- app/src/index.js
+++ app/src/index.js
@@ -1,6 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { Provider } from "react-redux";
+
 import "./index.css";
 import App from "./App";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+import store from "./store";
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById("root")
+);
