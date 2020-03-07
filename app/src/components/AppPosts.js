cf716ed38b0b40f4978e68b7b1fe95f74efab282
Your name <a.m.verdine@gmail.com>
2020-03-07 09:22:32 -0500

    Build out FETCH request in componentWIllMount/Map over and render FETCH response

--- app/src/components/AppPosts.js
+++ app/src/components/AppPosts.js
@@ -1,17 +1,30 @@
 import React, { Component } from "react";
 
 class AppPosts extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      posts: []
+    };
+  }
 
-conponentWillMount() {
-  fetch("https://jsonplaceholder.typicode.com/posts"); 
-    .then(res => res.json())
-    .then(data => console.log(data));
-}
+  componentWillMount() {
+    fetch("https://jsonplaceholder.typicode.com/posts")
+      .then(res => res.json())
+      .then(data => this.setState({ posts: data }));
+  }
 
   render() {
+    const postItems = this.state.posts.map(post => (
+      <div key={post.id}>
+        <h3>{post.title}</h3>
+        <p>{post.body}</p>
+      </div>
+    ));
     return (
       <div>
         <h1>Posts</h1>
+        {postItems}
       </div>
     );
   }
