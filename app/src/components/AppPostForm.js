a4246cbe5da2fc7d91b45e41b87b4d79f6aeb296
Your name <a.m.verdine@gmail.com>
2020-03-07 11:26:23 -0500

    Build form in AppPostForm component

--- app/src/components/AppPostForm.js
+++ app/src/components/AppPostForm.js
@@ -12,6 +12,21 @@ class AppPostForm extends Component {
     return (
       <div>
         <h1>Add Post</h1>
+        <form>
+          <div>
+            <label>Title: </label>
+            <br />
+            <input type="text" name="title" />
+          </div>
+          <br />
+          <div>
+            <label>Body: </label>
+            <br />
+            <textarea name="body" />
+            <br />
+            <button type="submit">Submit</button>
+          </div>
+        </form>
       </div>
     );
   }
