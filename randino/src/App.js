35a1adb1300c5d28c224f01a1c1de1239f9694e0
Troopy <40128411+troopaloop8@users.noreply.github.com>
2020-04-15 10:45:08 +0900

    more imports but got that CORS problem

--- randino/src/App.js
+++ randino/src/App.js
@@ -1,11 +1,18 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { createStore, applyMiddleware } from "redux";
 import {Provider} from "react-redux";
 import thunk from 'redux-thunk';
-
+import axios from 'axios'
 import './App.css';
+import { getFirstImageURL } from 'first-image-search-load';
 
 function App() {
+
+  useEffect(() => {
+    axios.get(getFirstImageURL(`computer`))
+    .then(res => console.log(res))
+  })
+
   return (
     <div className="App">
       Randino
--- randino/src/actions/index.js
+++ randino/src/actions/index.js
@@ -1,6 +1,17 @@
-
+import axios from 'axios';
+import ranDino from '../dinoRandomizer';
 
 export const FETCH_DATA = "FETCH_DATA";
 export const DATA_SUCCESS = "DATA_SUCCESS";
 export const DATA_ERROR = "DATA_ERROR";
 
+
+
+export const fetchData = () => dispatch => {
+    dispatch({
+        type: FETCH_DATA
+    });
+    setTimeout(() => {
+
+    }, 1000)
+}
