242c7b4081f89b39e5104f4c6d708245a9cc35b9
Golden Eagle <joowoon.kang@biola.edu>
2020-04-15 15:12:30 -0700

    met the mvp

---
+++ react-redux-app/src/components/ProgrammingQuotes.js
@@ -0,0 +1,48 @@
+import React, { useEffect } from 'react';
+import { connect } from 'react-redux';
+import Loader from 'react-loader-spinner';
+import "./ProgrammingQuotes.css"
+
+import { fetchQuote } from '../store/actions/quoteActions';
+
+const ProgrammingQuotes = props => {
+  useEffect(() => {
+    // call an action creator
+    props.fetchQuote();
+    console.log(props.fetchQuote)
+  }, []);
+
+  // if (props.isFetching) {
+  //   return <Loader />;
+  // }
+
+  return (
+    <div className="container">
+      <h1>Programming Quotes 🎙</h1>
+      {props.isFetching && (
+        <Loader type="Puff" color="#00BFFF" height={100} width={100} />
+      )}
+      {props.quote &&<div className="text">
+                      <h3>"{props.quote}"</h3>
+                      <h4>{props.author}</h4>
+                     </div>}
+      {props.error && <p className="error">{props.error}</p>}
+      <button onClick={props.fetchQuote}>Fetch a new quote</button>
+    </div>
+  );
+};
+
+const mapStateToProps = state => {
+  console.log(state);
+  return {
+    quote: state.quotes.quote,
+    isFetching: state.quotes.isFetching,
+    error: state.quotes.error,
+    author:state.quotes.author
+  };
+};
+
+export default connect(
+  mapStateToProps,
+  { fetchQuote }
+)(ProgrammingQuotes);
---
+++ react-redux-app/src/store/reducers/index.js
@@ -0,0 +1,6 @@
+import {combineReducers} from 'redux'
+import {quotesReducer as quotes} from './quotesReducer'
+
+export default combineReducers({
+    quotes
+})
