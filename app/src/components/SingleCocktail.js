ddba011145db99e0d17bff34229f39e465ba0ca4
anthony amaro <anthonyamaro@anthonys-MacBook-Pro.local>
2020-04-15 13:28:54 -0700

    adding routing to my app

--- app/src/components/SingleCocktail.js
+++ app/src/components/SingleCocktail.js
@@ -1,6 +1,10 @@
 import React from "react";
+import { Link, useRouteMatch } from "react-router-dom";
 
 const SingleCocktail = () => {
+  const { path, url } = useRouteMatch();
+
+  console.log(url);
   return (
     <div className="single-cocktail">
       <div className="img-container">
@@ -8,6 +12,13 @@ const SingleCocktail = () => {
           src="https://www.thecocktaildb.com/images/media/drink/wpxpvu1439905379.jpg"
           alt=""
         />
+      </div>
+      <div className="information">
+        <h3>name</h3>
+
+        <div className="btn">
+          <Link to={`/des/23`}>Read More</Link>
+        </div>
       </div>
     </div>
   );
--- app/src/index.js
+++ app/src/index.js
@@ -1,5 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { BrowserRouter } from "react-router-dom";
 import { createStore, applyMiddleware } from "redux";
 import { Provider } from "react-redux";
 import logger from "redux-logger";
@@ -14,7 +15,9 @@ const store = createStore(rootReducer, applyMiddleware(thunk, logger));
 
 ReactDOM.render(
   <Provider store={store}>
-    <App />
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </Provider>,
   document.getElementById("root")
 );
