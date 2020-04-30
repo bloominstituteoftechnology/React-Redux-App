94d76ef99ac1dd8ac00561edb1d76de44eb4ab6a
Ariel_Rodriguez <32606708+Ariels713@users.noreply.github.com>
2020-04-30 00:38:59 -0400

    finished, shipped

--- app/src/store/actions/formActions.js
+++ app/src/store/actions/formActions.js
@@ -1,4 +1,4 @@
-import Axios from 'axios'
+import Axios from "axios";
 export const SEARCH_TERM = "SEARCH_TERM";
 
 export const FETCH_PEXEL_IMAGE_START = "FETCH_PEXEL_IMAGE_START";
@@ -9,56 +9,69 @@ export const FETCH_WEATHER_DATA_START = "FETCH_WEATHER_DATA";
 export const FETCH_WEATHER_DATA_FAILED = "FETCH_WEATHER_DATA_FAILED";
 export const FETCH_WEATHER_SUCESS = "FETCH_WEATHER_SUCESS";
 
-export const searchFeature = searchTerm => {
-    console.log("%c SEARCH TERM", "color:green", searchTerm)
-    return dispatch => {
-        dispatch({type:SEARCH_TERM, payload:searchTerm})
-        dispatch({ type: FETCH_PEXEL_IMAGE_START });
-        Axios.get(
-            `https://pixabay.com/api/?key=14377008-6a15e1087d3bad2acb1cd8486&q=${searchTerm}&image_type=photo`
-          )
-            .then((res) => {
-              //Returns Random Image from arry
-              const data = res.data.hits;
-              const randomImage = data[Math.floor(Math.random() * data.length)];
-              //Returns Random Image from array
-              dispatch({ type: FETCH_PEXEL_IMAGE_SUCCESS, payload: randomImage });
-            })
-            .catch((err) => {
-              dispatch({ type: FETCH_PEXEL_IMAGE_FAIL, payload: `${err.data}` });
-            });
-    }
-}
+export const searchFeature = (searchTerm) => {
+  console.log("%c SEARCH TERM", "color:green", searchTerm.cityName);
+  return (dispatch) => {
+    dispatch({ type: SEARCH_TERM, payload: searchTerm });
+    dispatch({ type: FETCH_PEXEL_IMAGE_START });
+    dispatch({ type: FETCH_WEATHER_DATA_START });
+    Axios.get(
+      `https://pixabay.com/api/?key=14377008-6a15e1087d3bad2acb1cd8486&q=${searchTerm.cityName}&image_type=photo`
+    )
+      .then((res) => {
+        //Returns Random Image from arry
+        const data = res.data.hits;
+        const randomImage = data[Math.floor(Math.random() * data.length)];
+        //Returns Random Image from array
+        dispatch({ type: FETCH_PEXEL_IMAGE_SUCCESS, payload: randomImage });
+      })
+      .catch((err) => {
+        dispatch({ type: FETCH_PEXEL_IMAGE_FAIL, payload: `${err.data}` });
+      });
+    Axios.get(
+      `http://api.openweathermap.org/data/2.5/weather?appid=f9039d5e70b79bc54e8913e14e31a84f&q=${searchTerm.cityName}&units=imperial`
+    )
+      .then((res) => {
+        dispatch({ type: FETCH_WEATHER_SUCESS, payload: res.data });
+      })
+      .catch((err) => {
+        dispatch({
+          type: FETCH_WEATHER_DATA_FAILED,
+          payload: `${err.message}`,
+        });
+      });
+  };
+};
 
 // export const searchFeature = (searchTerm) => {
 //   return (dispatch) => {
-    // dispatch({ type: FETCH_PEXEL_IMAGE_START });
+// dispatch({ type: FETCH_PEXEL_IMAGE_START });
 //     dispatch({ type: FETCH_WEATHER_DATA_START });
-    
-    // Axios.get(
-    //   `https://pixabay.com/api/?key=14377008-6a15e1087d3bad2acb1cd8486&q=${searchTerm}&image_type=photo`
-    // )
-    //   .then((res) => {
-    //     //Returns Random Image from arry
-    //     const data = res.data.hits;
-    //     const randomImage = data[Math.floor(Math.random() * data.length)];
-    //     //Returns Random Image from array
-    //     dispatch({ type: FETCH_PEXEL_IMAGE_SUCCESS, payload: randomImage });
-    //   })
-    //   .catch((err) => {
-    //     dispatch({ type: FETCH_PEXEL_IMAGE_FAIL, payload: `${err.data}` });
-    //   });
-//     Axios.get(
-//       `http://api.openweathermap.org/data/2.5/weather?appid=f9039d5e70b79bc54e8913e14e31a84f&q=${searchTerm}&units=imperial`
-//     )
-//       .then((res) => {
-//         dispatch({ type: FETCH_WEATHER_SUCESS, payload: res.data });
-//       })
-//       .catch((err) => {
-//         dispatch({
-//           type: FETCH_WEATHER_DATA_FAILED,
-//           payload: `${err.message}`,
-//         });
-//       });
+
+// Axios.get(
+//   `https://pixabay.com/api/?key=14377008-6a15e1087d3bad2acb1cd8486&q=${searchTerm}&image_type=photo`
+// )
+//   .then((res) => {
+//     //Returns Random Image from arry
+//     const data = res.data.hits;
+//     const randomImage = data[Math.floor(Math.random() * data.length)];
+//     //Returns Random Image from array
+//     dispatch({ type: FETCH_PEXEL_IMAGE_SUCCESS, payload: randomImage });
+//   })
+//   .catch((err) => {
+//     dispatch({ type: FETCH_PEXEL_IMAGE_FAIL, payload: `${err.data}` });
+//   });
+// Axios.get(
+//   `http://api.openweathermap.org/data/2.5/weather?appid=f9039d5e70b79bc54e8913e14e31a84f&q=${searchTerm}&units=imperial`
+// )
+//   .then((res) => {
+//     dispatch({ type: FETCH_WEATHER_SUCESS, payload: res.data });
+//   })
+//   .catch((err) => {
+//     dispatch({
+//       type: FETCH_WEATHER_DATA_FAILED,
+//       payload: `${err.message}`,
+//     });
+//   });
 //   };
 // };
