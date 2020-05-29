41c797e099803cd6a172981982f4a25a6c976db0
Tatiana Zhizhimontova <tatyana.zhi@gmail.com>
2020-05-29 13:27:10 -0700

    styling cat card in cat page is added

--- app/src/components/Cat.js
+++ app/src/components/Cat.js
@@ -3,7 +3,14 @@ import React, { useEffect } from "react";
 import { useRouteMatch } from "react-router-dom";
 import { connect } from "react-redux";
 import { getImage } from "../actions";
-import { Spinner } from "reactstrap";
+import {
+  Spinner,
+  Card,
+  CardBody,
+  CardTitle,
+  CardSubtitle,
+  CardImg,
+} from "reactstrap";
 import Button from "@material-ui/core/Button";
 import { Container, Row, Col } from "reactstrap";
 
@@ -22,54 +29,59 @@ const Cat = (props) => {
   const catItem = props.breeds.find((breed) => breed.id === match.params.catID);
   console.log("catItem image", catItem);
   return (
-    <Container style={{ width: "100%" }}>
-      <Row style={{ marginTop: "1rem" }}>
-        <Col xs="6" md="6" xl="6">
-          {props.isFetchingData ? (
-            <div style={{ margin: "6rem auto" }}>
-              <Spinner
-                color="primary"
-                style={{ width: "3rem", height: "3rem" }}
-              />{" "}
-            </div>
-          ) : (
-            <div>
-              <img
-                alt="cat"
-                src={catItem.image}
-                style={{ width: "600px" }}
-              ></img>
-            </div>
-          )}
+    <Container className="themed-container" fluid={true}>
+      <Row style={{ margin: "0", marginTop: "1rem", marginBottom: "1rem" }}>
+        <Col xs="12" md="6" xl="6">
+          <Card style={{ border: "none" }}>
+            {props.isFetchingData ? (
+              <div style={{ margin: "6rem auto" }}>
+                <Spinner
+                  color="primary"
+                  style={{ width: "3rem", height: "3rem" }}
+                />{" "}
+              </div>
+            ) : (
+              <div>
+                <CardImg alt="cat" src={catItem.image} />
+              </div>
+            )}
+          </Card>
         </Col>
-        <Col xs="6" md="6" xl="4" style={{ marginLeft: "7rem" }}>
-          <div>
-            <h1 style={{ textAlign: "center" }}>{catItem.name}</h1>
-            <p> {catItem.description}</p>
-            <p>Life span: {catItem.life_span} years</p>
-            <p>Dog friendly level: {catItem.dog_friendly} </p>
-            <p>Energy level: {catItem.energy_level}</p>
-            <p>Hypoallergenic level: {catItem.hypoallergenic}</p>
-            <p>Hairless level: {catItem.hairless}</p>
-            <p>
-              Wikipedia:{" "}
-              <a
-                href={catItem.wikipedia_url}
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                {catItem.wikipedia_url}
-              </a>
-            </p>
-            <Button
-              style={{ with: "60%", marginTop: "2rem" }}
-              onClick={handleGetImg}
-              variant="contained"
-              color="primary"
-            >
-              {`Refresh photo of ${catItem.name} breed`}
-            </Button>
-          </div>
+
+        <Col xs="12" md="6" xl="6">
+          <Card style={{ padding: "2rem", border: "none" }}>
+            <div>
+              <h1 style={{ textAlign: "center" }}>{catItem.name}</h1>
+              <p> {catItem.description}</p>
+              <p>Life span: {catItem.life_span} years</p>
+              <p>Dog friendly level: {catItem.dog_friendly} </p>
+              <p>Energy level: {catItem.energy_level}</p>
+              <p>Hypoallergenic level: {catItem.hypoallergenic}</p>
+              <p>Hairless level: {catItem.hairless}</p>
+              <p>
+                Wikipedia:{" "}
+                <a
+                  href={catItem.wikipedia_url}
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  {catItem.wikipedia_url}
+                </a>
+              </p>
+              <div style={{ marginLeft: "25%", marginTop: "2rem" }}>
+                <Button
+                  style={{
+                    with: "60%",
+                  }}
+                  onClick={handleGetImg}
+                  variant="contained"
+                  color="primary"
+                >
+                  {`Refresh photo of ${catItem.name} breed`}
+                </Button>
+              </div>
+            </div>
+          </Card>
         </Col>
       </Row>
     </Container>
