5f952d2d950f404fad1b8e1596535d4d001d292e
Brian Clayton <finalb@projectfinalboss.com>
2020-05-29 15:57:49 -0400

    reducer setup

--- app/src/actions/index.js
+++ app/src/actions/index.js
@@ -2,13 +2,19 @@ import axios from 'axios';
 
 //API:  https://graphql.anilist.co/api/v2/
 
+export const START = "START";
+export const SUCCESS = 'SUCCESS';
+export const FAILURE = 'FAILURE';
+
 const animeApi = data => dispatch => {
+
+    dispatch({type: START});
 
     axios.post("https://graphql.anilist.co/api/v2/", data)
     .then(res => {
-        dispatch({type: CALL_SUCCESS, payload: res.data});
+        dispatch({type: SUCCESS, payload: res.data});
     })
     .catch(err => {
-        dispatch({type: CALL_FAILURE, payload: err.response});
+        dispatch({type: FAILURE, payload: err.response});
     });
 };
---
+++ app/src/reducers/index.js
@@ -0,0 +1,36 @@
+import {START, SUCCESS, FAILURE} from '../actions/index';
+
+const initialState = {
+
+    isLoading: false,
+    anime: [],
+    error: ""
+
+};
+
+export const reducer = (state = initialState, action) => {
+
+    switch(action.type){
+        case START:
+            return {
+                ...state,
+                isLoading: true
+
+            }
+            case SUCCESS:
+                return {
+                    ...state,
+                    isLoading: false,
+                    anime: action.payload
+                }
+            case FAILURE: 
+            return {
+                ...state,
+                isLoading: false,
+                error: action.payload
+            }
+            default:
+                return state;
+
+    }
+}
