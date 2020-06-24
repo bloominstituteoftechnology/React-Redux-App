3006a205b430c64e4ff094b321e4bab2b3662931
Stephen Goodrick <sgoodrickjr@gmail.com>
2020-06-24 14:50:57 -0400

    wrapped with Provider

--- app/src/App.js
+++ app/src/App.js
@@ -1,4 +1,10 @@
 import React from 'react';
+import ReactDOM from 'react-dom';
+import { createStore, applyMiddleware } from 'redux';
+import thunk from 'redux-thunk';
+import { appReducer } from './reducers';
+import { Provider } from 'react-redux';
+
 import logo from './logo.svg';
 import './App.css';
 
@@ -7,17 +13,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        
       </header>
     </div>
   );
--- app/src/index.js
+++ app/src/index.js
@@ -5,9 +5,9 @@ import App from './App';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
-  <React.StrictMode>
+  <Provider store={store}>
     <App />
-  </React.StrictMode>,
+  </Provider>,
   document.getElementById('root')
 );
 
