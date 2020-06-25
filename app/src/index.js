bc918bbce2efeda101d6c483bda0570d79ac2df7
Josh Riddle <riddlej9879@yahoo.com>
2020-06-25 17:40:27 -0500

    Added styling

--- app/src/index.js
+++ app/src/index.js
@@ -1,18 +1,18 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
-import { createStore, applyMiddleware } from 'redux';
-import { Provider } from 'react-redux'
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+import { createStore, applyMiddleware } from "redux";
+import { Provider } from "react-redux";
 import thunk from "redux-thunk";
 
-import { indexReducer } from './reducer/IndexReducer'
-import './index.css';
+import { personReducer } from "./reducer/personReducer";
+import "./index.css";
 
-const store = createStore(indexReducer, applyMiddleware(thunk))
+const store = createStore(personReducer, applyMiddleware(thunk));
 
 ReactDOM.render(
   <Provider store={store}>
     <App />
   </Provider>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
