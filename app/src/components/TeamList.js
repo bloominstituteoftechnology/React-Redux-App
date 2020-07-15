a57e212e60f37028d337396bab70c0a1f3405b5c
oscar-lopez-projects <d1lopeztv@gmail.com>
2020-07-15 14:03:25 -0700

    connected redux and mapestatetoprops

--- app/src/components/TeamList.js
+++ app/src/components/TeamList.js
@@ -3,11 +3,19 @@ import {connect} from 'react-redux';
 import {fetchTeams} from '../store/actions';
 
 
-const TeamList = ()=>{
+const TeamList = props =>{
     return( 
         <div>
             <h1>NBA TEAMS</h1>
         </div>
    )
+};
+
+const mapStatetoProps = state =>{
+  return{
+    isLoading: state.isLoading,
+    teams: state.teams,
+    error:state.error
+  }
 }
-export default TeamList;
+export default connect(mapStatetoProps, fetchTeams)(TeamList);
