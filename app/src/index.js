c3ff8dc839e40b11a1685ab36dfde1e90a86b852
Christopher W Schultz <chris.william.schultz@gmail.com>
2020-07-15 18:03:39 -0600

    typo

--- app/src/index.js
+++ app/src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
-import { createStore, applyMiddleware } from "redux"
+import { createStore, applyMiddleware } from "redux";
 import { Provider } from "react-redux";
 import thunk from 'redux-thunk';
 
