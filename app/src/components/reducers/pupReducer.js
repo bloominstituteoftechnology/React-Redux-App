8a64823e1a6ea6c4a3f419e5f2ed3d97597e8675
JenVest2020 <jenvest2019@gmail.com>
2020-07-28 12:41:52 -0500

    set up redux& dependencies

---
+++ app/src/components/reducers/pupReducer.js
@@ -0,0 +1,13 @@
+
+
+export const initialState = {
+    pups: [],
+    isFetching: false,
+    error: ''
+
+}
+
+export const pupReducer = (state = initialState, action) => {
+
+    return state
+}
--- app/src/index.js
+++ app/src/index.js
@@ -1,17 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+
+import { applyMiddleware, createStore } from 'redux';
+import { Provider } from 'react-redux';
+import thunk from 'redux-thunk';
+
 import './index.css';
 import App from './App';
-import * as serviceWorker from './serviceWorker';
+import { pupReducer } from './components/reducers/pupReducer';
+
+const store = createStore(pupReducer, applyMiddleware(thunk));
+
 
 ReactDOM.render(
-  <React.StrictMode>
+  <Provider store={store}>
     <App />
-  </React.StrictMode>,
+  </Provider>,
   document.getElementById('root')
 );
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
