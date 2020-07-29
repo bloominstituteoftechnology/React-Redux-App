dbb9c49da490c57278d6f9c6da8c21a256394146
dustin <dustnclaytiner@gmail.com>
2020-07-28 21:19:47 -0500

    installs done

---
+++ thunkapp/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div >
+
+    </div>
+  );
+}
+
+export default App;
---
+++ thunkapp/src/index.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import * as serviceWorker from './serviceWorker';
+import {applyMiddleware, createStore} from 'redux';
+import logger from 'redux-logger'
+import rootReducer from './reducers/rootReducer'
+
+const store = createStore(
+  rootReducer,
+  applyMiddleware(thunk)
+);
+
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://bit.ly/CRA-PWA
+serviceWorker.unregister();
