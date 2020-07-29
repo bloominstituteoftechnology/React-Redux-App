336716359b4f0c4cbf596b950207556bef452065
Darren Tebo <darrentebo83@outlook.com>
2020-07-28 21:49:08 -0400

    completed video background (stretch goal)

--- app/src/App.js
+++ app/src/App.js
@@ -1,18 +1,12 @@
 import React from 'react';
 import './App.css';
 
-import Header from './components/header/Header';
-import Weather from './components/weather/Weather';
-import Footer from './components/footer/Footer';
 import Background from './components/background/Background';
 
 function App() {
   return (
     <div className="App">
       <Background />
-      <Header />
-      <Weather />
-      <Footer />
     </div>
   );
 }
--- app/src/components/background/Background.js
+++ app/src/components/background/Background.js
@@ -2,15 +2,25 @@ import React from 'react';
 
 import sky from '../../resources/sky.mp4';
 
+import Header from '../header/Header';
+import Weather from '../weather/Weather';
+import Footer from '../footer/Footer';
+
 import './Background.css';
 
 const Background = props => {
     return(
+        <>
         <div className='background'>
+            <div className='color-overlay'></div>
             <video autoPlay loop muted>
                 <source src={sky} type="video/mp4" />
             </video>
         </div>
+        <Header />
+        <Weather />
+        <Footer />
+        </>
     );
 };
 
