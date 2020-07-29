da2a82200a496b79d38a211b2a776386c762e840
Darren Tebo <darrentebo83@outlook.com>
2020-07-28 20:29:01 -0400

    added weather data source credit

--- app/src/App.js
+++ app/src/App.js
@@ -3,12 +3,14 @@ import './App.css';
 
 import Header from './components/header/Header';
 import Weather from './components/weather/Weather';
+import Footer from './components/footer/Footer';
 
 function App() {
   return (
     <div className="App">
       <Header />
       <Weather />
+      <Footer />
     </div>
   );
 }
---
+++ app/src/components/footer/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Footer = props => {
+    return (
+        <div className='site-footer'>
+            <p>Weather data provided by <a href='https://www.metaweather.com/'>MetaWeather</a></p>
+        </div>
+    );
+};
+
+export default Footer;
