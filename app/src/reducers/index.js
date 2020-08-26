65a9675674a8b15616486f0f12d2a164ed51f081
noah niesen <noah@noahs-MacBook-Pro.local>
2020-08-25 19:21:12 -0500

    set up the dogReducer, imported React, useEffect, and connect in Dog.js

--- app/src/reducers/index.js
+++ app/src/reducers/index.js
@@ -12,11 +12,21 @@ export const FETCHING_DOG_FAILURE = "FETCHING_DOG_FAILURE";
 export const dogReducer = (state = initialState, action) => {
   switch(action.type) {
     case FETCHING_DOG_START:
-      return state;
+      return {
+        ...state,
+        isFetching: true
+      };
     case FETCHING_DOG_SUCCESS:
-      return state;
+      return {
+        ...state,
+        dog: action.payload,
+        isFetching: false
+      };
     case FETCHING_DOG_FAILURE:
-      return state;
+      return {
+        ...state,
+        isFetching: false
+      };
     default:
       return state;
   };
