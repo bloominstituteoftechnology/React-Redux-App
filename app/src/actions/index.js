536b8ff8d68a22f36f00d452574491c64a07c580
noah niesen <noah@noahs-MacBook-Pro.local>
2020-08-25 19:42:57 -0500

    changed the API url, added dispatch in the .then axios call

--- app/src/actions/index.js
+++ app/src/actions/index.js
@@ -5,9 +5,10 @@ export const getDog = () => (dispatch) => {
   dispatch({ type: FETCHING_DOG_START });
 
   axios
-    .get("https://dog.ceo/api/breeds")
+    .get("https://dog.ceo/api/breeds/image/random")
     .then((res) => {
-      console.log(res);
+      console.log(res.data.message);
+      dispatch({ type: FETCHING_DOG_SUCCESS, payload: res.data.message });
     })
     .catch((err) => {
       console.log(err);
