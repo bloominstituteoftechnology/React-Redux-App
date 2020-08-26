049c0878a86bfa476e6adae8490122f57f3e5023
noah niesen <noah@noahs-MacBook-Pro.local>
2020-08-25 19:47:19 -0500

    removed a console.log call in the axios .then call

--- app/src/actions/index.js
+++ app/src/actions/index.js
@@ -7,7 +7,6 @@ export const getDog = () => (dispatch) => {
   axios
     .get("https://dog.ceo/api/breeds/image/random")
     .then((res) => {
-      console.log(res.data.message);
       dispatch({ type: FETCHING_DOG_SUCCESS, payload: res.data.message });
     })
     .catch((err) => {
