fd519611a1bba74a68624c519deb3b6def2df438
noah niesen <noah@noahs-MacBook-Pro.local>
2020-08-25 19:32:36 -0500

    set up Dog.js

--- app/src/components/Dog.js
+++ app/src/components/Dog.js
@@ -1,3 +1,28 @@
 import React, { useEffect } from "react";
 import { connect } from "react-redux";
+import { getDog } from "../actions/index";
 
+const Dog = ({ getDog, dog, isFetching }) => {
+  useEffect(() => {
+    getDog();
+  }, [getDog]);
+
+  if (isFetching) {
+    return <h2>Loading...</h2>
+  };
+
+  return (
+    <>
+      <h2>Dog: {dog}</h2>
+      <button onClick={getDog}>Get new dog</button>
+    </>
+  );
+};
+
+const mapStateToProps = (state) => {
+  return { dog: state.dog };
+};
+
+const mapDispatchToProps = { getDog };
+
+export default connect(mapStateToProps, mapDispatchToProps)(Dog);
