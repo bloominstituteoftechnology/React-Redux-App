20951a8d805820af5d597dbad1cf88aaf004dc01
Donavan Terranova <dterran2@outlook.com>
2020-08-27 21:31:04 -0600

    finished

---
+++ react-redux-app/src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import './App.css';
+import Breeds from './components/breeds'
+
+function App() {
+
+
+
+  return (
+    <div className="App">
+      
+      <Breeds/>
+    </div>  
+  );
+
+}
+
+export default App;
