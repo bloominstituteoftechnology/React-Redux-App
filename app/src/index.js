d03f153d097f26ad34f3aaeb2e0e6cecab43e7d2
Fatima Rizvi <fatimarzv23@gmail.com>
2020-10-07 15:47:08 -0500

    set up most of index.js

--- app/src/index.js
+++ app/src/index.js
@@ -1,13 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+//import createStore and applyMiddleware
+import { createStore, applyMiddleware } from 'redux';
+import { Provider } from 'react-redux';
+import thunk from 'redux-thunk';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
+//add applyMiddleware to app 
+// const store = createStore(reducer, applyMiddleware(thunk));
+
 ReactDOM.render(
-  <React.StrictMode>
+  <Provider store = { store }>
     <App />
-  </React.StrictMode>,
+  </Provider>,
   document.getElementById('root')
 );
 
