ee1d83e3e4fc66428004bebf60f35985dd4fb677
unknown <tompsherman@gmail.com>
2020-10-07 23:40:07 -0400

    reset

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+import "./App.css";
+
+const App = () => {
+  return (
+    <div className="App">
+      <h1>Annual Country Holidays</h1>
+    </div>
+  );
+};
+
+export default App;
