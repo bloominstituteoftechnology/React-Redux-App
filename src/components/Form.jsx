6f59f701e773799b3c423ae3a57e061b9286249c
unknown <tompsherman@gmail.com>
2020-10-08 00:51:01 -0400

    search form

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,9 +1,25 @@
-import React from "react";
+import React, { useState } from "react";
 
 const Form = () => {
+  const [searchYear, setSearchYear] = useState("2020");
+
+  const changeHandle = (event) => {
+    setSearchYear(event.target.value);
+  };
+
   return (
     <div>
       <h2>this is the form</h2>
+      <form onSubmit={}>
+        <input
+          name="year"
+          type="text"
+          value={searchYear}
+          onChange={changeHandle}
+          placeholder="enter year"
+        />
+        <button>get holidays</button>
+      </form>
     </div>
   );
 };
