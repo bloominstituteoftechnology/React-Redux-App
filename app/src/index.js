12f7efe9d92c1e6368b8c9fad69420687ab0fb10
harvey magana <heyharv2012@gmail.com>
2020-10-10 09:35:04 -0700

    Store created and added to index.js

--- app/src/index.js
+++ app/src/index.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createStore } from 'redux';
+import reducers from './reducers';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
@@ -38,6 +40,8 @@ import * as serviceWorker from './serviceWorker';
 // 4. Create mapStateToProps
 // 5. Pass mapStateToProps into connect: 
 // export default connect (mapStateToProps, {})(Component)
+
+const store = createStore(reducers)
 
 ReactDOM.render(
   <React.StrictMode>
