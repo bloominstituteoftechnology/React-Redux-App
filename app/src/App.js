540621b3dcf9ae05082d987a76f20f6175d926e5
adelazalewski <paliuadela@yahoo.com>
2020-10-20 21:46:39 -0400

    created my app

---
+++ app/src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        
+    
+      </header>
+    </div>
+  );
+}
+
+export default App;
