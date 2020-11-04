c8eda2fed2606495adfa2db73fc9acc8f97672d2
Brandon Sievert (slick) <brandonwsievert@gmail.com>
2020-11-04 18:43:39 -0500

    Aquired NASA mars rover data that shows images taken the day before viewers date

---
+++ app/src/index.js
@@ -0,0 +1,24 @@
+// Libraries
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { createStore, applyMiddleware } from 'redux';
+import { Provider } from 'react-redux';
+import thunk from 'redux-thunk';
+import logger from 'redux-logger';
+
+// Redux (helpers)
+import { roverReducer } from './store/reducers';
+
+// Components
+import App from './App';
+
+// Stylesheets
+
+const store = createStore(roverReducer, applyMiddleware(thunk, logger));
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root')
+);
