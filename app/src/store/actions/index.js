065812dfbea3f3ecd85ce3da0c92ee210757f485
unknown <xiongjoh@gmail.com>
2020-11-04 14:39:19 -0600

    create app, create directories for actions and reducer

---
+++ app/src/store/actions/index.js
@@ -0,0 +1,2 @@
+
+const pokemonAPI = 'https://pokeapi.co/api/v2/pokemon?limit=25&offset=0'
---
+++ app/src/store/reducers/index.js
@@ -0,0 +1,9 @@
+
+const initialState = {
+    isLoading: false,
+    pokemonData: [],
+    error:""
+}
+
+
+
