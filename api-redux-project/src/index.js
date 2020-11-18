30bf64192a03f8624a0e74346c1817b0ce819efe
Osvaldo Lievano <Osvaldoalievano@gmail.com>
2020-11-17 22:52:22 -0500

    created store, called thunk, and initial reducer

--- api-redux-project/src/index.js
+++ api-redux-project/src/index.js
@@ -1,12 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import {Provider} from 'react-redux';
+import {reducer} from '../src/reducers/index'
+import {createStore,applyMiddleware} from 'redux'
+import thunk from 'redux-thunk'
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+const store = createStore(reducer,applyMiddleware(thunk))
+
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
---
+++ api-redux-project/src/reducers/index.js
@@ -0,0 +1,4 @@
+
+export const reducer = (state,action) =>{
+    console.log('ol: reducers: index.js : state,action',state,action)
+}
