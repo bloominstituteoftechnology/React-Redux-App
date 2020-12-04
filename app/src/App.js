f84a900f84b9f019f578913c11fc3239d6ea4269
Brian Abeyta-Pratt <babeytapratt@gmail.com>
2020-12-03 23:00:20 -0700

    add build

--- app/src/App.js
+++ app/src/App.js
@@ -2,6 +2,12 @@ import React from 'react';
 import Quote from './components/Quote'
 import './index.css'
 
+function url(path) {
+  return process.env.NODE_ENV === 'development'
+  ? `'http://localhost:5000${path}`
+  : path
+}
+
 export default function App() {
   return (
     <div className="App">
