6c8d11a024ae1752d2d5948bdc2a53c2e9769628
Katie Olson <katieolson84@gmail.com>
2020-12-09 13:45:19 -0700

    setup of app

---
+++ app/src/reducers/index.js
@@ -0,0 +1,13 @@
+
+const initialState = {
+    gif: '',
+    isFetching: false,
+    error: ''
+}
+
+export const reducer = (state = initialState, action) => {
+    switch(action.type) {
+        default:
+            return state;
+    }
+}
