cd3d03afa640a5a9984eb3b640e5213e30903d28
Jonathan Hsu <jonhsu.design@gmail.com>
2020-12-28 16:07:30 -0800

    began migrating API endpoints from v3 -> v4

--- app/src/actions/fetchHistory.js
+++ app/src/actions/fetchHistory.js
@@ -7,7 +7,7 @@ export const HISTORY_FAILURE = 'HISTORY_FAILURE';
 export const fetchHistory = () => dispatch => {
   dispatch({ type: HISTORY_START })
   axios
-    .get('https://api.spacexdata.com/v3/history')
+    .get('https://api.spacexdata.com/v4/history')
     .then(res => {
       dispatch({ type: HISTORY_SUCCESS, payload: res.data })
     })
