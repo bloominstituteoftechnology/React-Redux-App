fa6bc9ed182dc785ebef54c92155da9e8c029439
tbbcoach <58452640+tbbcoach@users.noreply.github.com>
2021-01-05 19:54:41 -0500

    composed reducer.

--- app/src/Reducers/quoteReducer.js
+++ app/src/Reducers/quoteReducer.js
@@ -4,3 +4,28 @@ const initialState = {
     quote: '',
     error: ''
 }
+
+export const quoteReducer = (state = initialState, action) => {
+    switch (action.type) {
+        case "FETCHING_QUOTE_START":
+            return {
+                ...state,
+                isFetching: true
+            }
+        case "FETCHING_QUOTE_START":
+            return {
+                ...state,
+                isFetching: false,
+                error: '',
+                quote: action.payload
+            }
+        case "FETCHING_QUOTE_ERROR":
+            return {
+                ...state,
+                isFetching: false,
+                error: action.payload
+            }
+        default:
+            return state;
+    }
+}
