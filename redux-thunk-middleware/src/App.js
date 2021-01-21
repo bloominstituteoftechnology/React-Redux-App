614852b48d3c67562a522fe01b09882dde44d4cd
Jacob Olness <jacobolness@Jacobs-MBP.lan>
2021-01-20 20:15:26 -0700

    MVP

---
+++ redux-thunk-middleware/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import Facts from './components/Facts';
+
+import './App.css';
+
+function App() {
+  return (
+   <div className="App">
+     <h1>🧠Learn something🧠</h1>
+    <Facts/>
+   </div>
+  );
+}
+
+export default App;
