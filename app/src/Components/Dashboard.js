3aff9b6c8deae2058a198458577e8e8154dfe755
Greg Wilson <gjwilson7390@gmail.com>
2021-01-30 13:24:24 -0600

    generic profile pic added to dashboard

--- app/src/Components/Dashboard.js
+++ app/src/Components/Dashboard.js
@@ -1,9 +1,25 @@
-import React, {useEffect} from 'react'
+import React, {useEffect, useState} from 'react'
 import { connect } from 'react-redux'
 
 import { getName } from '../Actions'
 
+import { Image, CloudinaryContext } from 'cloudinary-react'
+
+import genericProfilePic from '../genericprofilepic.png'
+
 function Dashboard (props) {
+
+    // componentDidMount() {
+    //     const s = document.createElement('script')
+    //     s.type = 'text/javascript'
+    //     s.async = true
+    //     s.src = 
+
+    // }
+
+    const [profilePic, setProfilePic] = useState(false)
+
+    // cloudinary.setCloudName('')
 
     useEffect( () => {
 
@@ -17,6 +33,11 @@ function Dashboard (props) {
         <div>
             <br></br><br></br>
             <h2>Welcome {props.first_name} {props.last_name}!</h2>
+            <br></br><br></br>
+            {profilePic ? 
+            <Image cloudName='chaqar-bible-app' />
+            : <img id="genericProfilePic" src={genericProfilePic}></img>
+            }
         </div>
     )
 }
