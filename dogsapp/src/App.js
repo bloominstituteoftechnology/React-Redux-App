ecdb96457b056911d74ebd9225efd416f96718a4
George Hatzigeorgio <geogehatzigeorgio@gmail.com>
2021-02-18 06:40:22 -0500

    adding css not done

--- dogsapp/src/App.js
+++ dogsapp/src/App.js
@@ -7,10 +7,13 @@ import Images from "./images"
 function App() {
   return (
     <div className="App">
-      <div className="header">
-          <img src="/src/dog-face_1f436.png" alt="dog-enoji"/>
-          <h1>Want to see some random pics of German Sheperds cause they are so stinkin cute!</h1>
-      </div>
+      <div className="header-image">
+          <img src="https://i.pinimg.com/originals/28/72/96/28729681636cddc44d56cb54b7931910.jpg" alt="dog-emoji"/>
+        </div>
+        <div className="heading">
+          <h1>HERE ARE SOME RANDOM PICTURES OF GERMAN SHEPERDS BECAUSE THEY ARE SO STINKIN CUTE!</h1>
+          </div>
+    
       <div>
         <Images/>
       </div>
--- dogsapp/src/images.js
+++ dogsapp/src/images.js
@@ -1,59 +1,42 @@
 import React from "react";
-import {connect} from "react-redux"
-import { getImages} from "./actions/actions"
+import { connect } from "react-redux";
+import { getImages } from "./actions/actions";
 
-const Images = (props) =>{
+const Images = (props) => {
+  const { images, isFetching, error, getImages } = props;
+  console.log(props);
 
-    const {images,isFetching,error,getImages} = props;
-    console.log(props)
+  const onClick = () => {
+    props.getImages();
+  };
 
-    const onClick =() =>{
-        props.getImages()
-    }
+  if (error) {
+    return <h3>Houston We have a problem!</h3>;
+  }
 
+  if (isFetching) {
+    return <h3>Fetching German Sheperds ! </h3>;
+  }
 
-    if (error){
-        return <h3>Houston We have a problem!</h3>
-    }
-
-    if (isFetching){
-        return <h3>Fetching German Sheperds ! </h3>
-    }
-
-
-
-
-
-return (
+  return (
     <>
-    <div>
-        <button onClick={onClick}>
-        </button>
-     </div>
-         {
-             props.images.map(image => (
-             
-            
-             <div>
-              <img src={image}/>
-             </div>
-             
-         ))
-         }
-    </>    
-)
+      <div>
+        <button onClick={onClick}>Click here for German Sheperds!!!</button>
+      </div>
+      {props.images.map((image) => (
+        <div>
+          <img src={image} />
+        </div>
+      ))}
+    </>
+  );
+};
+const mapStateToProps = (state) => {
+  return {
+    images: state.images,
+    isFetching: state.isFetching,
+    error: state.error,
+  };
+};
 
-
-
-
-
-}
-const mapStateToProps = (state) =>{
-    return{
-        images:state.images,
-        isFetching: state.isFetching,
-        error: state.error
-    }
-}
-
-export default connect(mapStateToProps,{getImages})(Images)
+export default connect(mapStateToProps, { getImages })(Images);
