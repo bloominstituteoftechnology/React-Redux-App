75fe4c3452742eb86cb263fdba241bd527cd600d
Saeed Khan <saeedkhan@Saeeds-MacBook-Pro.local>
2021-03-02 17:49:26 -0700

    store, logger, and thunk has been added

--- chuck-norris/src/index.js
+++ chuck-norris/src/index.js
@@ -2,16 +2,21 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+import reducer from './reducer/index'
+import { createStore, applyMiddleware } from 'redux'
+import logger from 'redux-logger'
+import thunk from 'redux-thunk'
+import { Provider } from 'react-redux'
+
+// Creating store 
+
+const store = createStore(reducer, applyMiddleware(logger, thunk));
 
 ReactDOM.render(
-  <React.StrictMode>
+  <Provider store={store}>
     <App />
-  </React.StrictMode>,
+  </Provider>,
   document.getElementById('root')
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+
